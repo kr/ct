a775e2bc889c819a5d3b70794ca66a38b4fe839f
Joshua Smith <jsmit272@jhu.edu>
2011-12-01 19:15:09 -0500

    Refactored netTestRunner and added generic protocol system. It works with a manually created net-test file

--- ct/netTestRunner.c
+++ ct/netTestRunner.c
@@ -9,20 +9,17 @@
 #include <arpa/inet.h>
 #include <stdbool.h>
 #include <syslog.h>
-#include <assert.h>
+#include "ct.h"
+#include <stdio.h>
 
-typedef struct nodeInfo {
+typedef struct namedNode {
   char * name;
-  int socket;
-  struct sockaddr* addr;
-  socklen_t addrLen;
-} NodeInfo;
+  struct ctnode * node;
+} NamedNode;
 
-NodeInfo * createNode(NetConnection * connection);
+struct ctnode* getNodeByName(NamedNode *nodeList, int numNodes, char * name);
 
-NodeInfo * getNodeInfoByName(NodeInfo **nodeList, int numNodes, char * name);
-
-int runSocketTests(NetConnection * connections) {
+int runSocketTests(const struct protocol *prot, NetConnection * connections) {
 
   // Create counters
   int i;
@@ -31,17 +28,18 @@ int runSocketTests(NetConnection * connections) {
   //Count number of connections
   int numNodes = 0;
   conCounter = connections;
-  while(conCounter != NULL) {
+  while(conCounter->name != NULL) {
     numNodes++;
     conCounter++;
   }
 
-  NodeInfo **nodeList = calloc(numNodes, sizeof(NodeInfo*));
+  NamedNode *namedList = calloc(numNodes, sizeof(NamedNode));
   
   // Create sockets and ports for each function
   i = 0; conCounter = connections;
-  while(conCounter != NULL) {
-    nodeList[i] = createNode(conCounter);
+  while(conCounter->name != NULL) {
+    namedList[i].name = conCounter->name;
+    namedList[i].node = prot->createNode(conCounter->name);
     i++; conCounter++;
   }
 
@@ -49,20 +47,19 @@ int runSocketTests(NetConnection * connections) {
 
   // Fork and start each connection
   i = 0; conCounter = connections;
-  while(conCounter != NULL) {
+  while(conCounter->name != NULL) {
     pid_t pid = fork();
     assert(pid != -1);
     if(pid!=0) {
       pids[i] = pid;
     } else {
-      NodeInfo * myInfo = 
-        getNodeInfoByName(nodeList, numNodes, conCounter->name);
-      assert(myInfo != NULL);
-      NodeInfo * connectedInfo = 
-        getNodeInfoByName(nodeList, numNodes, conCounter->dest);
-      assert(connectedInfo != NULL);
-      return conCounter->function(myInfo->socket, 
-          connectedInfo->addr, connectedInfo->addrLen);
+      struct ctnode * fromNode = 
+        getNodeByName(namedList, numNodes, conCounter->name);
+      assert(fromNode != NULL);
+      struct ctnode * toNode = 
+        getNodeByName(namedList, numNodes, conCounter->dest);
+      assert(toNode != NULL);
+      return conCounter->function(fromNode, toNode);
     }
     i++; conCounter++;
   }
@@ -72,6 +69,9 @@ int runSocketTests(NetConnection * connections) {
   i = 0;
   while(i < numNodes) {
     assert(waitpid(pids[i], &status, 0) == pids[i]);
+    if(status != 0) {
+      return status;
+    }
     //TODO return any bad ones and give up note freeing things will result
     // in segfaults for still running processes
     i++;
@@ -80,51 +80,20 @@ int runSocketTests(NetConnection * connections) {
   // Free all connections
   i = 0;
   while(i < numNodes) {
-    free(nodeList[i]->addr);
-    free(nodeList[i]);
+    prot->deleteNode(namedList[i].node);
     i++;
   }
-  free(nodeList);
+  free(namedList);
   free(pids);
 
   return 0;
 }
 
-NodeInfo * createNode(NetConnection * connection) {
-  // Create socket
-  int mySocket = socket(AF_INET, SOCK_DGRAM, 0);
-  assert(mySocket != -1);
-
-  // Set up blocking on socket
-  struct timeval time;
-  time.tv_sec = 1;
-  time.tv_usec = 0;
-  socklen_t len = sizeof(time);
-  setsockopt(mySocket, SOL_SOCKET, SO_RCVTIMEO, &time, len);
-
-  // Create sockaddr
-  struct sockaddr_in * channel = calloc(1, sizeof(struct sockaddr_in));
-  channel->sin_family = AF_INET;
-  channel->sin_addr.s_addr = INADDR_ANY;
-  
-  // Bind socket to sockaddr
-  assert(bind(mySocket, (struct sockaddr *) channel, sizeof(*channel))==0);
-
-  //Create NodeInfo
-  NodeInfo * node = calloc(1, sizeof(NodeInfo));
-  node->name = connection->name;
-  node->socket = mySocket;
-  node->addr = (struct sockaddr *) channel;
-  node->addrLen = sizeof(*channel);
-
-  return node;
-}
-
-NodeInfo * getNodeInfoByName(NodeInfo **nodeList, int numNodes, char * name) {
+struct ctnode * getNodeByName(NamedNode *namedList, int numNodes, char * name) {
   int i = 0;
   while(i < numNodes) {
-    if(strcmp(nodeList[i]->name, name) == 0) {
-      return nodeList[i];
+    if(strcmp(namedList[i].name, name) == 0) {
+      return namedList[i].node;
     }
     i++;
   }
--- ct/netTestRunner.h
+++ ct/netTestRunner.h
@@ -2,18 +2,26 @@
 #define _NET_TEST_RUNNER_H
 
 #include <netinet/in.h>
+#include "protocol.h"
 
-//TODO potentially add server functions that don't take addrs
-typedef int(*clientFunction)(int socket, 
-                              struct sockaddr *destAddr,
-                              socklen_t destLen);
+// The function type to be specified in nettest files.
+// Should run some code at "from", sending to or receiving from "to".
+// Returns 0 if no errors were encountered.
+typedef int(*nettestFunction)(struct ctnode *from, struct ctnode *to);
 
+// Defines a node name which runs function and is connected to the node dest.
 typedef struct netConnection {
   char * name;
-  clientFunction function;
+  nettestFunction function;
   char * dest;
 } NetConnection;
 
-int runSocketTests(NetConnection * connections);
+
+// given a protocol's createNode and deleteNode functions
+// creates each node by calling createNode(connection->name)
+// for each connection in connections
+//   forks and calls connection->function(connection->name, connection->dest)
+// deletes each node by calling deleteNode(connection->name)
+int runSocketTests(const struct protocol *prot, NetConnection * connections);
 
 #endif
